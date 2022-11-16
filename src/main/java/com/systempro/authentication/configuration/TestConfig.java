7fbd145ae020c832e240ece0548b70e165d9bce4
Fernandolilo <fernando.sistenpro@outlook.com>
2022-11-16 09:23:38 -0300

    Configuration and security and permitAll, conf init and security

--- src/main/java/com/systempro/authentication/configuration/TestConfig.java
+++ src/main/java/com/systempro/authentication/configuration/TestConfig.java
@@ -1,4 +1,4 @@
-package com.systempro.authentication;
+package com.systempro.authentication.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
