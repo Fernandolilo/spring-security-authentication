1679107a7020c3ded2e5bc891e8813e03886826c
Fernandolilo <fernando.sistenpro@outlook.com>
2022-11-16 10:06:50 -0300

    Authendicated in memory

--- src/main/java/com/systempro/authentication/configuration/WebSecurityConfig.java
+++ src/main/java/com/systempro/authentication/configuration/WebSecurityConfig.java
@@ -1,21 +1,35 @@
 package com.systempro.authentication.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-		.httpBasic()
-		.and()
-		.authorizeHttpRequests()
-		.anyRequest()
-		.authenticated()
-		.and().csrf().disable();
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeHttpRequests().anyRequest().authenticated().and().csrf().disable();
 	}
+
+	// criando autenticação em memoria
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.
+		inMemoryAuthentication()
+		.withUser("fernando")
+		.password(passwordEncoder().encode("1234"))
+		.roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
